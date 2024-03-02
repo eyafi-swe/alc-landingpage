d2f9f89911642673fa0c99534eade4f18de6f442
sarwar-azmain-reza <sarwar15-2562@diu.edu.bd>
2024-03-02 15:56:05 +0600

    Initial

---
+++ src/Page/Sheard/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='text-white text-center p-10'>
+            <p>© 2023 All Rights Reserved | Gamers Bank</p>
+        </div>
+    );
+};
+
+export default Footer;
